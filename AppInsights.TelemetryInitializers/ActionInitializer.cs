12c46ced6504ffacf1d35b14927189fff99d187d
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-01-06 07:48:14 -0500

    Add Session & Action initializers.

---
+++ AppInsights.TelemetryInitializers/ActionInitializer.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppInsights.TelemetryInitializers
+{
+    /// <summary>Generic initializer invoking an action at initialization.</summary>
+    public class ActionInitializer : ITelemetryInitializer
+    {
+        private readonly Action<TelemetryContext> _action;
+
+        /// <summary>Construct an initializer with an action.</summary>
+        /// <param name="action">Action to invoke when initilizing a telemetry's context.</param>
+        public ActionInitializer(Action<TelemetryContext> action)
+        {
+            _action = action ?? throw new ArgumentNullException(nameof(action));
+        }
+
+        void ITelemetryInitializer.Initialize(ITelemetry telemetry)
+        {
+            _action(telemetry.Context);
+        }
+    }
+}
---
+++ AppInsights.TelemetryInitializers/SessionInitializer.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AppInsights.TelemetryInitializers
+{
+    /// <summary>Add a session to the context of every telemetry.</summary>
+    /// <remarks>
+    /// This is useful in non-web application (i.e. where no session is added by ASP.NET).
+    /// It allows to segregate different run of an executable for instance.
+    /// </remarks>
+    public class SessionInitializer : ITelemetryInitializer
+    {
+        private readonly string _id;
+
+        /// <summary>Construct an initializer with a random (GUID) session ID.</summary>
+        public SessionInitializer():this(Guid.NewGuid().ToString())
+        {
+        }
+
+        /// <summary>Construct an initializer with a session ID.</summary>
+        /// <param name="id">Session ID to attach to telemetry's context.</param>
+        public SessionInitializer(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                throw new ArgumentNullException(nameof(id));
+            }
+            _id = id;
+        }
+
+        void ITelemetryInitializer.Initialize(ITelemetry telemetry)
+        {
+            telemetry.Context.Session.Id = _id;
+        }
+    }
+}
